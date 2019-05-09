f50618d15e1e3976bdbd3f1d0424807463357e31
Paul Bovbel <paul@bovbel.com>
2019-05-09 10:24:00 -0400

    Reimplement wait_for_status

--- aiorospy/src/aiorospy/action.py
+++ aiorospy/src/aiorospy/action.py
@@ -33,54 +33,6 @@ class AsyncSimpleActionServer(SimpleActionServer):
         goal = self.accept_new_goal()
         future = asyncio.run_coroutine_threadsafe(self._execute(goal), self._loop)
 
-class AsyncGoalHandle:
-
-    def __init__(self, loop):
-        self._loop = loop
-        self._status = janus.Queue()
-        self._feedback = janus.Queue()
-        self._done = asyncio.Event()
-
-    async def feedback(self):
-        while True:
-            waiting = asyncio.create_task(self._done.wait())
-            feedback = asyncio.create_task(self._feedback.async_q.get())
-            done, pending = await asyncio.wait(
-                {waiting, feedback},
-                return_when=asyncio.FIRST_COMPLETED)
-            if feedback in done:
-                yield feedback.result()
-            elif waiting in done:
-                return
-            else:
-                raise RuntimeError("Unexpected termination condition")
-
-    async def wait_for_status(self, status):
-        while True:
-            received = await self._status.async_q.get()
-            if received == status:
-                return
-
-    async def done(self):
-        return self._done.wait()
-
-    def status(self):
-        return self._goal_handle.get_goal_status()
-
-    async def result(self):
-        return self._goal_handle.get_result()
-
-    def cancel(self):
-        self._goal_handle.cancel()
-
-    def _transition_cb(self, goal_handle):
-        self._status.sync_q.put(goal_handle.get_goal_status())
-        if goal_handle.get_comm_state() == CommState.DONE:
-            self._loop.call_soon_threadsafe(self._done.set)
-
-    def _feedback_cb(self, goal_handle, feedback):
-        self._feedback.sync_q.put(feedback)
-
     # def is_ready(self):
     #     """ cribbed from actionlib.ActionClient.wait_for_server """
     #     if self._client.last_status_msg:
@@ -112,20 +64,83 @@ class AsyncGoalHandle:
     #     return False
 
 
+class AsyncGoalHandle:
+
+    def __init__(self):
+        self._status = janus.Queue()
+        self._feedback = janus.Queue()
+        self._old_statuses = set()
+
+        self._done_event = asyncio.Event()
+        self._status_event = asyncio.Event()
+        asyncio.create_task(self._process_status())
+
+    async def feedback(self):
+        while True:
+            done_action = asyncio.create_task(self._done_event.wait())
+            new_feedback = asyncio.create_task(self._feedback.async_q.get())
+            done, pending = await asyncio.wait(
+                {done_action, new_feedback},
+                return_when=asyncio.FIRST_COMPLETED)
+            if new_feedback in done:
+                yield new_feedback.result()
+            elif done_action in done:
+                return
+            else:
+                raise RuntimeError("Unexpected termination condition")
+
+    async def wait_for_status(self, status):
+        while not self._done_event.is_set():
+            await self._status_event().wait()
+            if status in self._old_statuses:
+                return
+
+        raise RuntimeError(f"Action is done, will never reach status {status}")
+
+    async def done(self):
+        return self._done_event.wait()
+
+    def status(self):
+        return self._goal_handle.get_goal_status()
+
+    async def result(self):
+        return self._goal_handle.get_result()
+
+    def cancel(self):
+        self._goal_handle.cancel()
+
+    def _transition_cb(self, goal_handle):
+        self._status.sync_q.put(goal_handle.get_goal_status())
+
+    def _feedback_cb(self, goal_handle, feedback):
+        self._feedback.sync_q.put(feedback)
+
+    async def _process_status(self):
+        while not self._done_event.is_set():
+            new_status = await self._status.async_q.get()
+
+            if new_status not in self._old_statuses:
+                self._old_statuses.add(new_status)
+                self._status_event.set()
+                self._status_event.clear()
+
+            if self._goal_handle.get_comm_state() == CommState.DONE:
+                self._done_event.set()
+
+
 class AsyncActionClient:
 
     def __init__(self, name, action_spec):
         self.name = name
         self._client = ActionClient(name, action_spec)
 
-    def send_goal(self, goal, loop):
+    def send_goal(self, goal):
 
         # while not self.is_ready():
         #     await asyncio.sleep(0.1)
         self._client.wait_for_server() # TODO(pbovbel) replace with async wait
 
-        async_handle = AsyncGoalHandle(loop=loop)
-        print("sending goal")
+        async_handle = AsyncGoalHandle()
         sync_handle = self._client.send_goal(goal,
             transition_cb=async_handle._transition_cb,
             feedback_cb=async_handle._feedback_cb,
