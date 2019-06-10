872b89463a4a7b4b69a49936b12e630acebc76a7
Paul Bovbel <paul@bovbel.com>
2019-06-10 14:22:51 -0400

    Add useful helpers

---
+++ aiorospy/src/aiorospy/helpers.py
@@ -0,0 +1,51 @@
+import asyncio
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def stop_on_exception_handler(loop):
+    def handler(_, context):
+        loop.default_exception_handler(context)
+        loop.stop()
+    return handler
+
+
+async def stop_loop_on_exception(awaitable):
+    try:
+        return await awaitable
+    except asyncio.CancelledError:
+        raise
+    except Exception as e:
+        logger.exception(f"Uncaught exception in awaitable: {e}")
+        asyncio.get_running_loop().stop()
+
+
+# TODO(pbovbel) create stop on exception decorator?
+
+
+def cancel_all_tasks(loop):
+    to_cancel = asyncio.all_tasks(loop)
+    if not to_cancel:
+        return
+
+    for task in to_cancel:
+        task.cancel()
+
+    loop.run_until_complete(
+        asyncio.gather(*to_cancel, return_exceptions=True))
+
+    for task in to_cancel:
+        if task.cancelled():
+            continue
+        if task.exception() is not None:
+            loop.call_exception_handler({
+                'message': 'unhandled exception during loop shutdown',
+                'exception': task.exception(),
+                'task': task,
+            })
+
+
+def cancel_on_exception_handler(loop, context, task):
+    loop.default_exception_handler(context)
+    task.cancel()
