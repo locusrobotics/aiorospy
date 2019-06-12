eafe63743990ee4bd8163ccf37375ef84cfe977c
Paul Bovbel <paul@bovbel.com>
2019-06-11 22:59:47 -0400

    Remove SimpleActionClient and Server

--- aiorospy/src/aiorospy/action.py
+++ aiorospy/src/aiorospy/action.py
@@ -13,35 +13,6 @@ from .helpers import ExceptionMonitor
 logger = logging.getLogger(__name__)
 
 
-# TODO(pbovbel) remove simple clients
-class AsyncSimpleActionClient:
-
-    def __init__(self, ns, action_spec):
-        self._client = SimpleActionClient(ns, action_spec)
-        self._client.wait_for_server()
-
-    async def send_goal(self, goal):
-        self._client.send_goal(goal)
-
-        loop = asyncio.get_event_loop()
-        await loop.run_in_executor(None, self._client.wait_for_result)
-        return self._client.get_state(), self._client.get_result()
-
-
-# TODO(pbovbel) remove simple clients
-class AsyncSimpleActionServer(SimpleActionServer):
-
-    def __init__(self, ns, action_spec, execute, loop=None):
-        self._loop = loop if loop is not None else asyncio.get_event_loop()
-        self._execute = execute
-        super().__init__(ns, action_spec, False)
-        self.register_goal_callback(self._execute_cb)
-
-    def _execute_cb(self):
-        goal = self.accept_new_goal()
-        future = asyncio.run_coroutine_threadsafe(self._execute(goal), self._loop)
-
-
 class _AsyncGoalHandle:
 
     def __init__(self, name, loop=None):
