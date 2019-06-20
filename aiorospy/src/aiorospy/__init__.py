fbb6ae334e0fe7a806effced5727cf43ce699c0d
Paul Bovbel <paul@bovbel.com>
2019-06-20 10:40:03 -0400

    Export await_and_log

--- aiorospy/src/aiorospy/__init__.py
+++ aiorospy/src/aiorospy/__init__.py
@@ -1,5 +1,5 @@
 from .action import AsyncActionClient, AsyncActionServer
-from .helpers import ExceptionMonitor, cancel_on_exception, cancel_on_shutdown
+from .helpers import ExceptionMonitor, cancel_on_exception, cancel_on_shutdown, await_and_log
 from .service import AsyncService, AsyncServiceProxy
 from .topic import AsyncPublisher, AsyncSubscriber
 
