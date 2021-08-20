da34cf0c1452ced03f587835bf2c1887c47eea73
Doug Smith <douglas.smith@swri.org>
2021-08-20 16:57:56 -0500

    Check python version with setuptools

--- aiorospy/setup.py
+++ aiorospy/setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
+import sys
+
+if sys.version_info < (3,5):
+    sys.exit('Sorry, Python < 3.5 is not supported')
 
 setup_args = generate_distutils_setup(
     packages=[
         'aiorospy'
     ],
-    package_dir={'': 'src'})
+    package_dir={'': 'src'},
+    python_requires='>3.5.0')
 
 setup(**setup_args)
