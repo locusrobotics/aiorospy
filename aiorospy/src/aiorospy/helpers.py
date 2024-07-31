75843a387df8199a7b478ff33e210213dcbea6bb
Michael Johnson <mjohnson459@gmail.com>
2024-07-31 18:56:43 +0100

    Fix async generator dependency (#46)
    * Fix naming of async generator dependency
    
    * Use contextlib if available

--- aiorospy/src/aiorospy/helpers.py
+++ aiorospy/src/aiorospy/helpers.py
@@ -4,10 +4,15 @@ import functools
 import logging
 import subprocess
 import time
+import sys
 
 import janus
 import rospy
-from async_generator import asynccontextmanager
+
+if sys.version_info >= (3, 10):
+    from contextlib import asynccontextmanager
+else:
+    from async_generator import asynccontextmanager
 
 logger = logging.getLogger(__name__)
 
